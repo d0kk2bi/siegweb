6fe6c0e83d4abd5b6d1daf991a0fc6d2d5da4457
kwon99 <kyonggi@kakao.com>
2022-08-07 17:04:26 +0900

    black으로 코드 포매팅 과정 수행 (#8)

--- client/client.py
+++ client/client.py
@@ -1,8 +1,8 @@
 import pymysql, hashlib, time, os
 
-team = ""   # Team name
-user = ""   # User name (mysql name)
-passwd = "" # User passwd (mysql password)
+team = ""  # Team name
+user = ""  # User name (mysql name)
+passwd = ""  # User passwd (mysql password)
 
 now = time.localtime()
 hour = now.tm_hour
@@ -11,11 +11,11 @@ key = "hacking_homepage_13st_%s_%s_%s_" % (team, user, "%02d" % hour)
 
 # make flags
 enc = hashlib.md5()
-enc.update((key+"server").encode("utf-8"))
+enc.update((key + "server").encode("utf-8"))
 flag1 = "flag{" + enc.hexdigest() + "}"
 
 enc = hashlib.md5()
-enc.update((key+"db").encode("utf-8"))
+enc.update((key + "db").encode("utf-8"))
 flag2 = "flag{" + enc.hexdigest() + "}"
 
 
@@ -26,11 +26,11 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 
 with open("/flag/flag", "w", encoding="UTF8") as f:
-    f.write(flag1+"\n")
+    f.write(flag1 + "\n")
 
 
 # save flag2 in database
-db = pymysql.connect(host="localhost",user=user,password=passwd,db="flag")
+db = pymysql.connect(host="localhost", user=user, password=passwd, db="flag")
 
 cursor = db.cursor()
 
