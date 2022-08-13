7991fddd32ca39eb242f362ee29ebe4bc7c4e34d
kwon99 <kyonggi@kakao.com>
2022-08-13 14:56:59 +0900

    flag 취약점 보완 & server sql 정리 및 리팩토링 (#11, #12, #13)

--- server/server.py
+++ server/server.py
@@ -1,17 +1,18 @@
 import sqlite3, hashlib, time, json, random
 
-class Server:
 
+class Server:
     def __init__(self, path):
         # db 연결
         self.conn = sqlite3.connect(
             f"/var/lib/docker/overlay2/{path}/merged/opt/CTFd/CTFd/ctfd.db"
         )
+
         self.cur = self.conn.cursor()
 
         # 계정 정보 불러오기
-        with open('user.json', 'r') as f:
-            self.user = json.load(f)
+        with open("user.json", "r") as f:
+            self.user_dict = json.load(f)
 
         # hidden 방지
         self.cur.execute("update challenges set state='hidden'")
@@ -22,24 +23,126 @@ class Server:
 
     # challenge 혹은 flag의 마지막 번호 찾기
     def select_last(self, condition):
-        self.cur.execute(
-            f"select id from {condition} order by id desc limit 1"
-            )
+        self.cur.execute(f"select id from {condition} order by id desc limit 1")
         try:
             return (self.cur.fetchone())[0]
         except TypeError:
             return 0
 
     # FLAG 생성
-    def generate_flag(self, username, condition):
+    def generate_flag(self, user, condition):
         enc = hashlib.md5()
         random_key = random.random
-        key = f"kknock_hack_def_{str(random_key)}_{self.user[username]}_{self.time_key}"
+        key = "kknock_hack_def_%s_%s_%s_" % (
+            str(random_key),
+            self.user_dict[user],
+            self.time_key,
+        )
 
         enc.update((key + condition).encode("utf-8"))
 
         return f"flag{{{enc.hexdigest()}}}"
 
- 
-if "__name__" == "__main__":
-    server = Server()
+    # 사용자 별 FLAG 생성해서 json에 저장
+    def flag_save(self):
+        # 이후에 db, server에 flag 넣을 때 사용해야 하므로 self 지정
+        self.flag_dict = {}
+
+        # 딕셔너리에 데이터베이스, 서버의 FLAG 추가
+        for user in self.user_dict:
+            self.flag_dict[user] = [
+                self.generate_flag(user, "db"),
+                self.generate_flag(user, "server"),
+            ]
+
+        # json 파일에 쓰기
+        with open("flag.json", "w") as f:
+            json.dump(self.flag_dict, f, indent=4)
+
+    # challenge 생성
+    def db_setting_challenge(self):
+        sql = (
+            "insert into challenges (id, name, description, max_attempts, "
+            "values, category, type, state) values "
+        )
+
+        # db challenge 생성
+        for key in self.user_dict:
+            try:
+                sql += "(%d, '%s', '%s', 0, '%d', '%s', 'standard', 'visible')," % (
+                    self.user_dict[key][0] + self.select_last("challenges"),
+                    key + "_db",
+                    self.user_dict[key][2],
+                    100,
+                    self.user_dict[key][1],
+                )
+            except IndexError:
+                pass
+
+        # server challenge 생성
+        for key in self.user_dict:
+            try:
+                sql += "(%d, '%s', '%s' 0, %d, '%s', 'standard', 'visible')," % (
+                    self.user_dict[key][0]
+                    + self.select_last("challenges")
+                    + len(self.user_dict),
+                    key + "_server",
+                    self.user_dict[key][2],
+                    300,
+                    self.user_dict[key][1],
+                )
+            except IndexError:
+                pass
+
+        # 마지막 쉼표 제거
+        sql = sql[:-1]
+
+        # SQL 적용
+        self.cur.execute(sql)
+        self.conn.commit()
+
+    # FLAG 생성
+    def db_setting_flag(self):
+        sql = "insert into flags (id, challenge_id, type, content) values "
+
+        # db FLAG 생성
+        for key in self.user_dict:
+            try:
+                sql += "(%d, %d, '%s', '%s')," % (
+                    self.user_dict[key][0] + self.select_last("challenges"),
+                    self.user_dict[key][0] + self.select_last("challenges"),
+                    "static",
+                    self.flag_dict[key],
+                )
+            except IndexError:
+                pass
+
+        # server FLAG 생성
+        for key in self.user_dict:
+            try:
+                sql += "(%d, %d, '%s', '%s')," % (
+                    self.user_dict[key][0]
+                    + self.select_last("challenges")
+                    + len(self.user_dict),
+                    self.user_dict[key][0]
+                    + self.select_last("challenges")
+                    + len(self.user_dict),
+                )
+            except IndexError:
+                pass
+
+        # 마지막 쉼표 제거
+        sql = sql[:-1]
+
+        # SQL 적용
+        self.cur.execute(sql)
+        self.conn.commit()
+
+
+if __name__ == "__main__":
+    server = Server("952cd60c88ce6e741827917ade8023c757b03bb0e2611e15b2ba7d651f1b9c5e")
+
+    server.flag_save()
+
+    server.db_setting_challenge()
+    server.db_setting_flag()
