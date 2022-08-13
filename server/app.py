212983ddf8e5cc85527377b5d394adc800d81e21
kwon99 <kyonggi@kakao.com>
2022-08-13 16:35:38 +0900

    기본적인 기능 구현 (#15)

---
+++ server/app.py
@@ -0,0 +1,30 @@
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# 계정 정보 가져오기
+with open("json/user.json", "r") as u:
+    user_dict = json.load(u)
+
+
+@app.route("/", methods="GET")
+def flag():
+    token = request.args.get("token")
+
+    # 사용자 정보 찾기
+    try:
+        for key, value in user_dict.items():
+            if value[3] == token:
+                user = key
+            break
+
+    except:
+        return "Incorrect Token!"
+
+    # FLAG 정보 불러오기
+    with open("json/flag.json", "r") as f:
+        flag_dict = json.load(f)
+
+    return flag_dict[user][0] + flag_dict[user][1]
+
