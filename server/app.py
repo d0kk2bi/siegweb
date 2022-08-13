fdce9364597bd2e098c3ea0af1474043ab81fa04
kwon99 <kyonggi@kakao.com>
2022-08-13 16:59:45 +0900

    기본적인 기능 구현 (#15)

--- server/app.py
+++ server/app.py
@@ -8,23 +8,28 @@ with open("json/user.json", "r") as u:
     user_dict = json.load(u)
 
 
-@app.route("/", methods="GET")
+@app.route("/", methods=["GET"])
 def flag():
     token = request.args.get("token")
+    user = ""
 
     # 사용자 정보 찾기
-    try:
-        for key, value in user_dict.items():
-            if value[3] == token:
-                user = key
-            break
+    for key, value in user_dict.items():
+        if value[3] == token:
+            user = key
+        break
 
-    except:
-        return "Incorrect Token!"
+    # 값이 제대로 들어갔는지 확인
+    if user == "":
+        return "Incorrect Token"
 
     # FLAG 정보 불러오기
     with open("json/flag.json", "r") as f:
         flag_dict = json.load(f)
 
     return flag_dict[user][0] + flag_dict[user][1]
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
 
